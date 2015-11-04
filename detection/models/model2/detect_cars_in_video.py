9ec85512036bdff60f33bdbc4cd9d37d5c0c2fa1
oraviv <oraviv@paypal.com>
2015-11-04 16:06:55 +0200

    asd

--- detection/models/model2/detect_cars_in_video.py
+++ detection/models/model2/detect_cars_in_video.py
@@ -1,11 +1,31 @@
 # From: http://python.developermemo.com/106_23244752/
 import cv2
 import numpy as np
+#capture = cv2.VideoCapture('../../imgs/VID_20150916_165227.mp4')
+capture = cv2.VideoCapture('../../imgs/VID_20151104_114549.mp4')
+#capture = cv2.VideoCapture('../../imgs/VID_20151104_140110.mp4')
+
+
+# def simpleBGSubtruct():
+#     gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+#     gray = cv2.GaussianBlur(gray, (21, 21), 0)
+#     
+#     # if the average frame is None, initialize it
+#     if avg is None:
+#         print "[INFO] starting background model..."
+#         avg = gray.copy().astype("float")
+#         #rawCapture.truncate(0)
+#         return
+#     
+#     cv2.accumulateWeighted(gray, avg, 0.5)
+#     frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))  
+    
+    
 #backsub = cv2.BackgroundSubtractorMOG()
 backsub = cv2.createBackgroundSubtractorMOG2()
-#capture = cv2.VideoCapture('../../imgs/VID_20150916_165227.avi')
-capture = cv2.VideoCapture('../../imgs/VID_20151104_114549.mp4')
+#backsub = cv2.createBackgroundSubtractorKNN()
 
+avg = None
 best_id = 0
 i = 0
 if capture:
@@ -17,11 +37,11 @@ if capture:
         #print frame0.shape
         frame2 = np.array(frame0[800:1500:1,400:1080:1])
 
-        #cv2.imshow("Track", frame2)
-
         if ret:
         
             fgmask = backsub.apply(frame2, None, 0.01)
+            
+            #simpleBGSubtruct()
             
             #_, contours, hierarchy = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
             _, contours, hierarchy = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -42,3 +62,4 @@ if capture:
         key = cv2.waitKey(10)
         if key == ord('q'):
                 break
+ 
